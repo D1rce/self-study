329745a684bdeff40d2e3569c68e069b5cb60d0f
D1rce <476610900@qq.com>
2018-01-31 13:58:53 +0800

    Create Main2.java

---
+++ Java/hello/src/main/java/test/Main2.java
@@ -0,0 +1,43 @@
+package study01.pkg.domain;
+
+public class Main2 {
+
+    /*
+     * 
+     */
+    public static void main(String[] args) {
+        
+        System.err.println(testClosure().get());
+        
+        
+    }
+
+//    public static Supplier<Integer> testClosure() {
+//
+//        final int i = 1;
+//
+//        return new Supplier<Integer>() {
+//
+//            @Override
+//            public Integer get() {
+//
+//                return i;
+//
+//            }
+//
+//        };
+//
+//    }
+    public static Supplier<Integer> testClosure() {
+
+        int i = 1;
+
+        return () -> {
+
+            return i;
+
+        };
+
+    }
+    
+}
